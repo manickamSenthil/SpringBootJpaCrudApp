ecd03a6aeedcdf1582cad065f318361d8d0d41df
Senthil Manickam.s <senthilmanickam.sundarapanilappan@collinsongroup.com>
2020-11-06 15:11:18 +0800

    application name and port number changes

--- src/main/java/com/crudapp/model/Pets.java
+++ src/main/java/com/crudapp/model/Pets.java
@@ -21,7 +21,7 @@ public class Pets {
 	private String petAge;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public long getId() {
 		return id;
 	}
